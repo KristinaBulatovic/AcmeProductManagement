a830ad59e15a3c911bf3551ef6f739faf2a30090
Kristina Bulatovic <kristina.bulatovic@itlab.local>
2018-10-03 10:51:22 +0200

    Update all

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './poduct'
+import { ProductService } from './product.service';
 
 @Component({
-    selector:'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list-component.css']
 })
@@ -12,6 +12,7 @@ pageTitle: string = 'Product List';
 imageWidth: number = 50;
 imageMargin: number = 2;
 showImage: boolean = false;
+errorMessage: string;
 
 _listFilter: string;
 get listFilter(): string {
@@ -23,33 +24,10 @@ set listFilter(value:string){
 }
 
 filteredProducts: IProduct[];
-products: IProduct[] = [
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden...",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "https://fyf.tac-cdn.net/images/products/large/F-692.jpg"
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "https://res.cloudinary.com/ufn/image/upload/c_pad,dpr_1.5,f_auto,fl_progressive,h_449,w_400/mtzii15kfcgkfnlhc8mp.jpg"
-    }
-];
+products: IProduct[] = [];
 
 
-constructor(){
-    this.filteredProducts = this.products;
-    this.listFilter = 'cart';
+constructor(private productService: ProductService){
 }
 
 onRatingClicked(message: string): void {
@@ -67,7 +45,13 @@ toggleImage():void{
 }
 
 ngOnInit(): void{
-    console.log('In onInit');
+    this.productService.getProducts().subscribe(
+        products =>{
+            this.products = products;
+            this.filteredProducts = this.products;
+        },
+    error => this.errorMessage = <any>error 
+    );
 }
 
 }
